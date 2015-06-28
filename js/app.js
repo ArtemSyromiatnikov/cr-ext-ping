ba91df8ecaf69b87f9faa389450e50eee0178b81
Humanity <true.master.yoda@gmail.com>
2015-06-28 18:21:26 +0100

    App v0.2

---
+++ js/app.js
@@ -0,0 +1,79 @@
+/// <reference path="typings/angularjs/angular.d.ts"/>
+angular.module('pingApp', [])
+	.constant("CONST", {
+		quality: {
+			good: 'good',
+			avg: 'avg',
+			bad: 'bad',
+			none: 'none'
+		}
+	})
+	.factory("probeFactory", function(CONST, $http) {
+		var probeData = [],
+			probes = [];
+		return {
+			createProbe: function(probeData) {
+				return {
+					data: probeData,
+					inProgress: false,
+					quality: CONST.quality.none,
+					ms: 0,
+					setMs: function(value) {
+						this.ms = value;
+						if (this.ms > 0 && this.ms < 500) {
+							this.quality = CONST.quality.good;
+						} else if ( this.ms < 1000) {
+							this.quality = CONST.quality.avg;
+						} else {
+							this.quality = CONST.quality.bad;
+						}
+					},
+					ping: function() {
+						var self = this, 
+							startTime = new Date().getTime();
+						this.inProgress = true;
+						$http.get(this.data.url)
+							.success(function(data, status) {
+								//console.log("SUCCESS: ", self.data.url, " - ", status);
+								self.setMs(new Date().getTime() - startTime);
+								self.inProgress = false;
+								
+							}).error(function(data, status) {
+								//console.warn("FAIL: ", self.data.url, " - ", status);
+								self.setMs(new Date().getTime() - startTime);
+								self.inProgress = false;
+							});
+					}
+				};
+			},
+			initProbes: function(probeDataArr) {
+				probeData = probeDataArr;
+				probes = probeData.map(this.createProbe, this);
+			},
+			getProbes: function() {
+				return probes;
+			}
+		};
+	})
+	.controller('pingListCtrl', function($scope, $http, probeFactory) {
+		var probes = [{
+				title: "Google",
+				url: "https://www.google.se/"
+			}, {
+				title: "Apple",
+				url: "https://www.apple.com/"
+			}, {
+				title: "Microsoft",
+				url: "https://www.microsoft.com/en-us/default.aspx"
+			}
+		];
+		probeFactory.initProbes(probes);
+		$scope.probes = probeFactory.getProbes();
+		
+		$scope.pingNow = function() {
+			$scope.probes.forEach(function(probe){
+				probe.ping();
+			}, this);
+		};
+		
+	});
