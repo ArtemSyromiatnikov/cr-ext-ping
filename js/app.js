8a33a32d9c049ae9e7ecf130571a0cd7c623319c
Humanity <true.master.yoda@gmail.com>
2015-07-07 21:09:39 +0100

    Persistence

--- js/app.js
+++ js/app.js
@@ -1,27 +1,5 @@
 /// <reference path="typings/angularjs/angular.d.ts"/>
 angular.module('pingApp', ['ngRoute'])
-	.config(function($routeProvider){
-		$routeProvider
-			.when('/', {
-				templateUrl: 'views/main.html',
-				controller: 'PingListCtrl'
-			})
-			.when('/config', {
-				templateUrl: 'views/config.html',
-				controller: 'ConfigCtrl'
-			})
-			.when('/ping/new', {
-				templateUrl: 'views/editPing.html',
-				controller: 'EditPingCtrl'
-			})
-			.when('/ping/:pingId/edit', {
-				templateUrl: 'views/editPing.html',
-				controller: 'EditPingCtrl'
-			})
-			.otherwise({
-				redirectTo: '/'
-			});
-	})
 	.constant("CONST", {
 		quality: {
 			good: 'good',
@@ -30,88 +8,143 @@ angular.module('pingApp', ['ngRoute'])
 			none: 'none'
 		}
 	})
-	.factory("probeFactory", function(CONST, $http) {
-		var probeData = [],
-			probes = [],
-			createProbe = function(probeData) {
-				return {
-					data: probeData,
-					inProgress: false,
-					quality: CONST.quality.none,
-					ms: 0,
-					setMs: function(value) {
-						this.ms = value;
-						if (this.ms > 0 && this.ms < 500) {
-							this.quality = CONST.quality.good;
-						} else if ( this.ms < 1000) {
-							this.quality = CONST.quality.avg;
-						} else {
-							this.quality = CONST.quality.bad;
-						}
-					},
-					ping: function() {
-						var self = this,
-							startTime = new Date().getTime();
-						this.inProgress = true;
-						$http.get(this.data.url)
-							.success(function(data, status) {
-								//console.log("SUCCESS: ", self.data.url, " - ", status);
-								self.setMs(new Date().getTime() - startTime);
-								self.inProgress = false;
+	.factory("utils", function() {
+		var Utils = {
+			isArray: function(a) {
+				return a && a.constructor === Array;
+			},
+			deepExtend: function(destination, source) {
+				for (var property in source) {
+					if (source[property] && source[property].constructor &&
+			     		source[property].constructor === Object) {
+			      		destination[property] = destination[property] || {};
+			      		arguments.callee(destination[property], source[property]);
+			    	} else {
+			      		destination[property] = source[property];
+			    	}
+			  	}
+			  	return destination;
+		  	},
+			deepCopy: function(source) {
+				if (source) {
+					return Utils.deepExtend({}, source);
+				}
+				return source;
+			}
+		};
+		return Utils;
+	})
+	.factory("probeFactory", function($http, utils) {
+		var ls = localStorage || {},
+			probeData,
 
-							}).error(function(data, status) {
-								//console.warn("FAIL: ", self.data.url, " - ", status);
-								self.setMs(new Date().getTime() - startTime);
-								self.inProgress = false;
-							});
-					}
+			getProbeData = function() {
+				if (ls.probeData) {
+					probeData = JSON.parse(ls.probeData);
+				}
+				return probeData || [];
+			},
+			saveProbeData = function() {
+				if (utils.isArray(probeData)) {
+					ls.probeData = JSON.stringify(probeData)
+				}
+			},
+			generateNewId = function() {
+				var maxId = probeData
+					.map(function(probe) { return probe.id })
+					.reduce(function(max, probeId) {
+						return Math.max(max, probeId);
+					}, 0);
+				console.log("new id: ", maxId + 1);
+				return maxId + 1;
+			},
+			// First-run initialization.
+			initialize = function(defaultProbes) {
+				if (ls.probeData) {
+					probeData = getProbeData();
+				} else {
+					var id = 1;
+					probeData = defaultProbes.map(function(probe) {
+						probe.id = id++;
+						return probe;
+					});
+					saveProbeData();
 				}
 			};
 
+		initialize(PROBES);
+
 		return {
-			initProbes: function(probeDataArr) {
-				probeData = probeDataArr;
-				probes = probeData.map(createProbe, this);
-			},
-			getPingDataList: function() {
-				return probeData;
-			},
 			getProbes: function() {
-				return probes;
+				return probeData.map(utils.deepCopy);
 			},
 			getProbe: function(id) {
-				var probe = probeData.filter(function(probe) { return probe.id === id; });
-				return probe[0];
+				var probe = probeData.filter(function(probe) { return probe.id === id; })[0];
+				return utils.deepCopy(probe);
 			},
 			createProbe: function(newProbe) {
-				newProbe.id = Math.ceil(Math.random()*1000000);
+				newProbe.id = generateNewId();
 				probeData.push(newProbe);
-				probes.push(createProbe(newProbe));
+
+				saveProbeData();
 			},
 			editProbe: function(id, data) {
-				var probe = this.getProbe(id);
-				if (probe) {
-					probe.title = data.title;
-					probe.url = data.url;
-				}
+				this.removeProbe(id);
+				this.createProbe(data);
+
+				saveProbeData();
 			},
 			removeProbe: function(id) {
 				var probe = this.getProbe(id),
 					probeIx = probeData.indexOf(probe);
 				probeData.splice(probeIx, 1);
-				probes.splice(probeIx, 1);
+
+				saveProbeData();
 			}
 		};
 	})
 	.controller('ConfigCtrl', function($scope, probeFactory) {
-		$scope.pingDataList = probeFactory.getPingDataList();
+		$scope.probes = probeFactory.getProbes();
 		$scope.remove = function(id) {
 			probeFactory.removeProbe(id);
 		}
 	})
-	.controller('PingListCtrl', function($scope, $http, probeFactory) {
-		probeFactory.initProbes(PROBES);
-		$scope.probes = probeFactory.getProbes();
+	.controller('PingListCtrl', function($scope, $http, CONST, probeFactory) {
+		var probeArr = probeFactory.getProbes();
+		$scope.probes = probeArr.map(function(probe) {
+			return {
+				data: probe,
+				inProgress: false,
+				quality: CONST.quality.none,
+				ms: 0,
+				setMs: function(value) {
+					this.ms = value;
+					if (this.ms > 0 && this.ms < 500) {
+						this.quality = CONST.quality.good;
+					} else if ( this.ms < 1000) {
+						this.quality = CONST.quality.avg;
+					} else {
+						this.quality = CONST.quality.bad;
+					}
+				},
+				ping: function() {
+					var self = this,
+						startTime = new Date().getTime();
+					this.inProgress = true;
+					$http.get(this.data.url)
+						.success(function(data, status) {
+							//console.log("SUCCESS: ", self.data.url, " - ", status);
+							self.setMs(new Date().getTime() - startTime);
+							self.inProgress = false;
+
+						}).error(function(data, status) {
+							//console.warn("FAIL: ", self.data.url, " - ", status);
+							self.setMs(new Date().getTime() - startTime);
+							self.inProgress = false;
+						});
+				}
+			}
+		});
 
 		$scope.pingNow = function() {
 			$scope.probes.forEach(function(probe){
@@ -148,4 +181,26 @@ angular.module('pingApp', ['ngRoute'])
 			}
 			$location.path("/config");
 		};
+	})
+	.config(function($routeProvider){
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/main.html',
+				controller: 'PingListCtrl'
+			})
+			.when('/config', {
+				templateUrl: 'views/config.html',
+				controller: 'ConfigCtrl'
+			})
+			.when('/ping/new', {
+				templateUrl: 'views/editPing.html',
+				controller: 'EditPingCtrl'
+			})
+			.when('/ping/:pingId/edit', {
+				templateUrl: 'views/editPing.html',
+				controller: 'EditPingCtrl'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
 	});
--- js/appData.js
+++ js/appData.js
@@ -1,14 +1,14 @@
 var PROBES = [
     {
-        id: 1,
+        //id: 1,
         title: "Apple",
         url: "https://www.apple.com/"
     },{
-        id: 2,
+        //id: 2,
         title: "Google",
         url: "https://www.google.se/"
     },{
-        id: 3,
+        //id: 3,
         title: "Microsoft",
         url: "https://www.microsoft.com/en-us/"
     }
